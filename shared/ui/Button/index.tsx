2985473e4e21f3fb14128b48cc545cb816a533b6
EnemyDeaD <dmitriy.yarmola@gmail.com>
2021-09-05 14:54:25 +0300

    changed button styles

--- shared/ui/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { styles } from './styles.css'
-
-interface PropsInterface {}
-export const Button: React.FC<PropsInterface> = ({ children }) => {
-	return <button className={styles}>{children}</button>
-}
